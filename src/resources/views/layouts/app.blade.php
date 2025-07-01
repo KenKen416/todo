afd4b9361d3a59b77eef5d74f10031f8ea6b6172
KenKen <kenkenstudy416@gmail.com>
2025-07-01 11:18:01 +0900

    Todoアプリ初級編終了

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,21 +3,26 @@
 
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>ToDo</title>
-  <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
-  <link rel="stylesheet" href="{{asset('css/common.css')}}">
- @yield('css')
+  <title>Todo</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+  <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+  @yield('css')
 </head>
 
 <body>
   <header class="header">
-    <header class="header__inner">
-      <a href="/" class="header__logo">Todo</a>
-    </header>
+    <div class="header__inner">
+      <a class="header__logo" href="/">
+        Todo
+      </a>
+    </div>
   </header>
+
   <main>
     @yield('content')
   </main>
 </body>
+
 </html>
